8a130926e748e51472ba22b1232acf13fe7df749
DiegoVega87 <diegovega@me.com>
2023-11-28 17:51:32 +0100

    Refactor App.js component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-
-
-import analyzeImage from './azure-image-analysis';
 import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Computer Vision</h1>
-        <input type="text" size="50" placeholder="Enter URL to analyze or textual prompt to generate an image" />
-        <div>
-          <button onClick={callAnalyzeImage}>Analyze</button>
-          <button>Generate</button>
-        </div>
+        <div>Insert URL or type prompt:</div>
+        <input type="text" size = "50" placeholder="Enter URL to analize or textual prompt to generate an image" />
+        <div><button onClick= "callAnalizeImage()"> Analyze</button> <button>Generate</button></div>
+
       </header>
     </div>
-
   );
 }
 
 
 function callAnalyzeImage() {
-  analyzeImage();
+  callAnalyzeImage();
 }
 
 export default App;
