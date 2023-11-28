7d79d0a09e1a42d6813a9eead96d22faa22c244d
DiegoVega87 <diegovega@me.com>
2023-11-28 17:52:57 +0100

    Fix onClick function name in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>Computer Vision</h1>
         <div>Insert URL or type prompt:</div>
         <input type="text" size = "50" placeholder="Enter URL to analize or textual prompt to generate an image" />
-        <div><button onClick= "callAnalizeImage()"> Analyze</button> <button>Generate</button></div>
+        <div><button onClick={callAnalyzeImage}> Analyze</button> <button>Generate</button></div>
 
       </header>
     </div>
