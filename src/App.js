7e6acda2dbc82b21fdb7c6dc694ec89b91e339de
DiegoVega87 <diegovega@me.com>
2023-11-28 17:42:01 +0100

    Update react-scripts version to 5.0.1 and refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+
+
 import analyzeImage from './azure-image-analysis';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Computer Vision</h1>
-        <div>Insert URL or type prompt:</div>
-        <input type="text" size = "50" placeholder="Enter URL to analize or textual prompt to generate an image" />
-        <div><button onClick= "callAnalizeImage()"> Analyze</button> <button>Generate</button></div>
-
+        <input type="text" size="50" placeholder="Enter URL to analyze or textual prompt to generate an image" />
+        <div>
+          <button onClick={callAnalyzeImage}>Analyze</button>
+          <button>Generate</button>
+        </div>
       </header>
     </div>
+
   );
 }
 
